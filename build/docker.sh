d13a3507ec668c0d57f6f4b9e2e65c795e94a60f
José María Viúdez <jmviudezp2003@gmail.com>
2023-02-13 19:18:47 +0100

    first commit

--- build/docker.sh
+++ build/docker.sh
@@ -1,5 +1,4 @@
 # Comandos Docker
-cd docker-compose/ubuntu/
 docker compose -f build/docker-compose/ubuntu/ub-node-nginx-nest.yml build --no-cache
 docker compose -f build/docker-compose/ubuntu/ub-node-nginx-nest.yml down
 docker compose -f build/docker-compose/ubuntu/ub-node-nginx-nest.yml up -d --remove-orphans
